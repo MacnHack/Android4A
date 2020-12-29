ad5653af924b34b4f5e6b4f72e0fbaa868590ae2
MacnHack <nathanmachet78@orange.fr>
2020-12-29 19:07:00 +0100

    Add Unit Test + Login(email) page

--- app/src/main/java/com/example/android4a/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/example/android4a/presentation/main/MainViewModel.kt
@@ -7,26 +7,28 @@ import com.example.android4a.domain.entity.User
 import com.example.android4a.domain.useCase.CreateUserUseCase
 import com.example.android4a.domain.useCase.GetUserUseCase
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainViewModel (
         private val createUserUseCase: CreateUserUseCase,
         private val getUserUseCase: GetUserUseCase
 ): ViewModel(){
 
-    val counter: MutableLiveData<Int> = MutableLiveData()
+    val loginLiveData: MutableLiveData<LoginStatus> = MutableLiveData()
 
-    init {
-        counter.value = 0
-    }
 
-    fun onClickedIncrement(emailUser: String) {
+    fun onClickedIncrement(emailUser: String, password: String) {
         viewModelScope.launch(Dispatchers.IO) {
-            createUserUseCase.invoke(User("test"))
-            delay(1000)
-            val user = getUserUseCase.invoke("test")
+            val user = getUserUseCase.invoke(emailUser)
+            val loginStatus = if(user != null){
+                LoginSuccess(user.email)
+            }else{
+                LoginError
+            }
+            withContext(Dispatchers.Main){
+                loginLiveData.value = loginStatus
+            }
         }
         //counter.value = (counter.value ?: 0) + 1
     }
